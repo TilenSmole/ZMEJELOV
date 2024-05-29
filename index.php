c8694a2beb8ace690e00080047a888e35f31d3fd
randomtuser <sspamt1@gmail.com>
2024-05-29 12:04:53 +0200

    t4ets

--- index.php
+++ index.php
@@ -2,40 +2,7 @@
 include('translations/load_translations.php');
 $translations = loadTranslations();
 
-require_once __DIR__.'/router.php';
 echo __DIR__ . "dir" ."\n";
-
-
-get('/', '/firstPage.php');
-get('/SHARED/header.php', '/SHARED/header.php');
-get('/translations/load_translations.php', '/translations/load_translations.php');
-get('/translations/getLanguage.php', '/translations/getLanguage.php');
-get('/SHARED/footer.php', '/SHARED/footer.php');
-get('/zmentures/introduction', '/zmentures#introduction_OG');
-get('/SERVER/getSessionData.php', '/SERVER/getSessionData.php');
-post(__DIR__.'/login', '/SERVER/login.php');
-get('../SHARED/header.php', '../SHARED/header.php');
-get('/SERVER/logout.php', '/SERVER/logout.php');
-post('/register', '/SERVER/register.php');
-get('/register', '/SERVER/register.php');
-get(__DIR__.'/login', '/SERVER/login.php');
-get('/language.php', '/language.php');
-get('/translations/language.php', '/translations/language.php');
-//get('/translations/language.php?language=en', '/translations/language.php?language=en');
-get('/translations/language.php?language', '/translations/language.php?language');
-//get('/translations/language.php?language=slo', '/translations/language.php?language=slo');
-get('/zmentures', '/zmejelov_basic.php');
-get('/Zmejelov1869', '/zmejelov_og.php');
-get('/Crackelov', '/zmejelov_speed_run.php');
-get('/TheFinalRage', '/zmejelov_mcqueen.php');
-get('/CityZmentures', '/ZMEJELOV_njam_njam.php');
-get('/intro', '/index.php');
-get('/user', '/user.php');
-post('/zmentures', '/zmejelov_basic.php');
-get('/contact', '/SHARED/contact.php');
-any('/404','/errors/404.php');
-
-
 
 
 ?>
